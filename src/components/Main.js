a3356244fb333f07eca4f155cc4ccef66b71f8e2
cr0c0 <hyde69ciel@gmail.com>
2024-05-25 23:57:23 +0900

    #240525 통합검색창 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,16 @@ import {
   MenuItem,
   FormControl,
   Select,
+  Input,
+  Button,
+  FormControlLabel,
+  Checkbox,
+  Stack,
 } from "@mui/material";
 import dayjs from "dayjs";
 import PlaceList from "./PlaceList";
-import { useUser } from "../UserContext";
-import { useRoute } from "../RouteContext";
 import { useSearchContext } from "../SearchContext";
+import { CheckBox } from "@mui/icons-material";
 
 function Main() {
   const {
@@ -24,111 +28,143 @@ function Main() {
     setPickedLocal_1,
     exhibition,
     popupstore,
+    facility,
   } = useSearchContext();
 
   return (
     <Container>
-      {searchInfo !== undefined ? (
-        <Grid container spacing={2}>
-          <Grid item>
-            <DatePicker
-              label="날짜"
-              format="YYYY-MM-DD"
-              value={dayjs(searchInfo.date)}
-              // minDate={today}
-              minDate={dayjs(new Date())}
-              onChange={(newValue) =>
-                setSearchInfo({ ...searchInfo, date: newValue })
-              }
-              // defaultValue={dayjs(
-              //   "2022-04-17"
-              //   //`${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
-              // )}
-            />
-          </Grid>
+      <Stack spacing={3}>
+        {searchInfo !== undefined ? (
+          <div>
+            <Grid container spacing={2}>
+              <Grid item>
+                <DatePicker
+                  label="날짜"
+                  format="YYYY-MM-DD"
+                  value={dayjs(searchInfo.date)}
+                  // minDate={today}
+                  minDate={dayjs(new Date())}
+                  onChange={(newValue) =>
+                    setSearchInfo({ ...searchInfo, date: newValue })
+                  }
+                  // defaultValue={dayjs(
+                  //   "2022-04-17"
+                  //   //`${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+                  // )}
+                />
+              </Grid>
 
-          {localcodes && pickedLocal_1 ? (
-            <Grid item>
-              <FormControl>
-                <InputLabel id="demo-simple-select-label">
-                  지역 대분류
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={pickedLocal_1}
-                  label="Age"
-                  onChange={(e) => {
-                    setPickedLocal_1(e.target.value);
+              {localcodes && pickedLocal_1 ? (
+                <Grid item>
+                  <FormControl>
+                    <InputLabel id="label_pickedLocal_1">
+                      지역 대분류
+                    </InputLabel>
+                    <Select
+                      labelId="label_pickedLocal_1"
+                      value={pickedLocal_1}
+                      label="Age"
+                      onChange={(e) => {
+                        setPickedLocal_1(e.target.value);
 
+                        setSearchInfo({
+                          ...searchInfo,
+                          localcode: localcodes.find(
+                            (obj) => obj.parentId === e.target.value
+                          ).id,
+                        });
+                      }}
+                    >
+                      {localcodes.map((localcode) =>
+                        localcode.localLevel === 0 ? (
+                          <MenuItem key={localcode.id} value={localcode.id}>
+                            {localcode.name}
+                          </MenuItem>
+                        ) : (
+                          ""
+                        )
+                      )}
+                    </Select>
+                  </FormControl>{" "}
+                  <FormControl>
+                    <InputLabel id="label_localcode">지역 소분류</InputLabel>
+                    <Select
+                      labelId="label_localcode"
+                      value={searchInfo.localcode}
+                      label="Age"
+                      onChange={(e) => {
+                        setSearchInfo({
+                          ...searchInfo,
+                          localcode: e.target.value,
+                        });
+                      }}
+                    >
+                      {localcodes.map((localcode) =>
+                        localcode.localLevel === 1 &&
+                        localcode.parentId === pickedLocal_1 ? (
+                          <MenuItem key={localcode.id} value={localcode.id}>
+                            {localcode.name}
+                          </MenuItem>
+                        ) : (
+                          ""
+                        )
+                      )}
+                    </Select>
+                  </FormControl>
+                </Grid>
+              ) : (
+                ""
+              )}
+              <Grid item>
+                <FormControl>
+                  <InputLabel>검색어</InputLabel>
+                  <Input type="text" id="input_keyword" />
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
                     setSearchInfo({
                       ...searchInfo,
-                      localcode: localcodes.find(
-                        (obj) => obj.parentId === e.target.value
-                      ).id,
+                      keyword: document.getElementById("input_keyword").value,
                     });
                   }}
                 >
-                  {localcodes.map((localcode) =>
-                    localcode.localLevel === 0 ? (
-                      <MenuItem key={localcode.id} value={localcode.id}>
-                        {localcode.name}
-                      </MenuItem>
-                    ) : (
-                      ""
-                    )
-                  )}
-                </Select>
-              </FormControl>{" "}
-              <FormControl>
-                <InputLabel id="demo-simple-select-label">
-                  지역 소분류
-                </InputLabel>
-                <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={searchInfo.localcode}
-                  label="Age"
-                  onChange={(e) => {
-                    setSearchInfo({ ...searchInfo, localcode: e.target.value });
-                  }}
-                >
-                  {localcodes.map((localcode) =>
-                    localcode.localLevel === 1 &&
-                    localcode.parentId === pickedLocal_1 ? (
-                      <MenuItem key={localcode.id} value={localcode.id}>
-                        {localcode.name}
-                      </MenuItem>
-                    ) : (
-                      ""
-                    )
-                  )}
-                </Select>
-              </FormControl>
+                  검색
+                </Button>
+              </Grid>
             </Grid>
-          ) : (
-            ""
-          )}
 
-          {exhibition != null ? (
-            <Grid item>
-              <PlaceList placeList={exhibition} type="exhibition" limit="4" />
-            </Grid>
-          ) : (
-            ""
-          )}
+            {exhibition !== null && exhibition.length !== 0 ? (
+              <div>
+                <PlaceList placeList={exhibition} type="exhibition" limit="4" />
+              </div>
+            ) : (
+              ""
+            )}
 
-          {popupstore != null ? (
-            <Grid item>
-              <PlaceList placeList={popupstore} type="popup" limit="4" />
-            </Grid>
-          ) : (
-            ""
-          )}
-        </Grid>
-      ) : (
-        ""
-      )}
+            {popupstore !== null && popupstore.length !== 0 ? (
+              <div>
+                <PlaceList placeList={popupstore} type="popup" limit="4" />
+              </div>
+            ) : (
+              ""
+            )}
+
+            {facility !== null && facility.length !== 0 ? (
+              <div>
+                <PlaceList placeList={facility} type="facility" limit="4" />
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        ) : (
+          ""
+        )}
+      </Stack>
     </Container>
   );
 }
