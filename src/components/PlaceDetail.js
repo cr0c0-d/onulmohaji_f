66c707e2382fa23a3db54faad97f6128548aa3d6
cr0c0 <hyde69ciel@gmail.com>
2024-05-27 22:58:49 +0900

    #240527 placeDetail 페이지 배치 개선

--- src/components/PlaceDetail.js
+++ src/components/PlaceDetail.js
@@ -84,69 +84,75 @@ function PlaceDetail({ placeType }) {
   };
 
   return (
-    <Container>
+    <Container sx={{ width: "100%" }}>
       {detail !== null ? (
-        <Grid container spacing={2}>
-          <Grid item>
-            <img
-              id="img_big"
-              src={detail.imageList[0]}
-              style={{ width: 300 }}
-              alt={detail.title}
-            />
-            {detail.imageList.length > 1 ? (
-              <div>
-                {detail.imageList.map((image, index) => (
-                  <img
-                    key={index}
-                    src={image}
-                    style={{ width: 80, cursor: "pointer" }}
-                    alt={index}
-                    onClick={() => {
-                      document
-                        .getElementById("img_big")
-                        .setAttribute("src", image);
-                    }}
-                  />
-                ))}
-              </div>
-            ) : (
-              ""
-            )}
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h3" component="div">
-              {detail.title}
-            </Typography>
-            {detail.subTitle ? (
-              <Typography gutterBottom variant="body1" component="div">
-                {detail.startDate} ~ {detail.endDate}
-              </Typography>
-            ) : (
-              ""
-            )}
-            <Typography gutterBottom variant="body1" component="div">
-              {detail.startDate} ~ {detail.endDate}
-            </Typography>
-            <Typography gutterBottom variant="body1" component="div">
-              장소 : {detail.place} ({detail.address}){" "}
-              {detail.placeUrl !== undefined && detail.placeUrl !== "" ? (
-                <Link to={detail.placeUrl}>홈페이지</Link>
+        <Stack>
+          <Grid container spacing={2} sx={{ display: "flex" }}>
+            <Grid item>
+              <img
+                id="img_big"
+                src={detail.imageList[0]}
+                style={{ width: 300 }}
+                alt={detail.title}
+              />
+              {detail.imageList.length > 1 ? (
+                <div>
+                  {detail.imageList.map((image, index) => (
+                    <img
+                      key={index}
+                      src={image}
+                      style={{ width: 80, cursor: "pointer" }}
+                      alt={index}
+                      onClick={() => {
+                        document
+                          .getElementById("img_big")
+                          .setAttribute("src", image);
+                      }}
+                    />
+                  ))}
+                </div>
               ) : (
                 ""
               )}
-            </Typography>
-            {detail.url !== "" ? (
-              <Typography gutterBottom variant="body1" component="div">
-                <Link to={detail.url}>링크2</Link>
+            </Grid>
+            <Grid item>
+              <Typography gutterBottom variant="h3" component="div">
+                {detail.title}
               </Typography>
-            ) : (
-              ""
-            )}
+              {detail.subTitle ? (
+                <Typography gutterBottom variant="body1" component="div">
+                  {detail.startDate} ~ {detail.endDate}
+                </Typography>
+              ) : (
+                ""
+              )}
+              <Typography gutterBottom variant="body1" component="div">
+                {detail.startDate} ~ {detail.endDate}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                장소 : {detail.place} ({detail.address}){" "}
+                {detail.placeUrl !== undefined && detail.placeUrl !== "" ? (
+                  <a href={detail.placeUrl} target="_blank">
+                    홈페이지
+                  </a>
+                ) : (
+                  ""
+                )}
+              </Typography>
+              {detail.url !== "" ? (
+                <Typography gutterBottom variant="body1" component="div">
+                  <a href={detail.url} target="_blank">
+                    링크2
+                  </a>
+                </Typography>
+              ) : (
+                ""
+              )}
+            </Grid>
           </Grid>
           <Grid item>
             <Stack spacing={3}>
-              {detail.contents1 !== null ? (
+              {detail.contents1 !== null && detail.contents1.length > 0 ? (
                 <Card>
                   <CardContent>
                     <div
@@ -158,7 +164,7 @@ function PlaceDetail({ placeType }) {
                 ""
               )}
               <br />
-              {detail.contents2 !== null ? (
+              {detail.contents2 !== null && detail.contents2.length > 0 ? (
                 <Card>
                   <CardContent>
                     <div
@@ -171,7 +177,10 @@ function PlaceDetail({ placeType }) {
               )}
 
               <Grid item>
-                <div id="map" style={{ width: "100%", height: "400px" }}></div>
+                <div
+                  id="map"
+                  style={{ minWidth: "100%", height: "400px" }}
+                ></div>
               </Grid>
 
               <Grid container>
@@ -184,7 +193,7 @@ function PlaceDetail({ placeType }) {
               </Grid>
             </Stack>
           </Grid>
-        </Grid>
+        </Stack>
       ) : (
         ""
       )}
