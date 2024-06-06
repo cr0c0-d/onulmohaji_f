d767da27f3919f025f34d271c33e8e5446dbeb20
cr0c0 <hyde69ciel@gmail.com>
2024-06-06 22:38:04 +0900

    #240606 일정 공유 참여 페이지

---
+++ src/components/RoutePermission.js
@@ -0,0 +1,145 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../UserContext";
+import { useAuthAPI } from "../AuthAPI";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import { useSearchContext } from "../SearchContext";
+
+export default function RoutePermission() {
+  const { userInfo } = useUser();
+  const [targetRoute, setTargetRoute] = useState(null);
+  const { searchInfo, setSearchInfo } = useSearchContext();
+
+  const history = useNavigate();
+  const AuthAPI = useAuthAPI();
+
+  // route 정보 조회
+  const getRouteInfo = async () => {
+    const response = await axios({
+      url: `${process.env.REACT_APP_API_ROOT}/api${window.location.pathname}`,
+      method: "GET",
+    }).catch((error) => {
+      console.log("route 조회 실패");
+      console.log(error);
+    });
+
+    if (response && response.status === 200) {
+      console.log(response.data);
+      setTargetRoute(response.data);
+    }
+  };
+
+  useEffect(() => {
+    getRouteInfo();
+  }, []);
+
+  const confirmUserInfo = () => {
+    // 로그인상태인지 확인
+    if (userInfo === null || userInfo.id === undefined) {
+      // 비로그인 -> 로그인 페이지로
+      history("/login", { state: { beforeUrl: window.location.pathname } });
+    }
+  };
+
+  useEffect(() => {
+    if (targetRoute !== null) {
+      confirmUserInfo();
+    }
+  }, [targetRoute]);
+
+  const addRoutePermission = () => {
+    AuthAPI({
+      url: "/api/route/permission",
+      method: "POST",
+      data: { routeId: targetRoute.routeId, userId: userInfo.id },
+      success: (result) => {
+        if (result.status === 201) {
+          setSearchInfo({ ...searchInfo, date: result.data.routeDate });
+          history("/");
+        }
+      },
+      fail: (error) => {
+        console.log(error);
+      },
+    });
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <GroupAddIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          일정 참여
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          //onSubmit={handleSubmit}
+          sx={{ mt: 3 }}
+        >
+          {targetRoute !== null ? (
+            targetRoute.memberList.find((obj) => obj === userInfo.id) !==
+            undefined ? (
+              <Stack spacing={3}>
+                <Typography component="body1" variant="body1">
+                  이미 참여중인 일정입니다.
+                </Typography>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => history("/")}
+                >
+                  메인화면으로
+                </Button>
+              </Stack>
+            ) : (
+              <Stack spacing={3}>
+                <Typography component="body1" variant="body1">
+                  {`${targetRoute.ownerName}님의 ${targetRoute.routeDate} 일정에 참가하시겠습니까?`}
+                </Typography>
+                <Typography component="body1" variant="body1">
+                  해당 날짜에 이미 일정이 존재하는 경우 덮어씌워집니다.
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={addRoutePermission}
+                >
+                  참가하기
+                </Button>
+
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => history("/")}
+                >
+                  메인화면으로
+                </Button>
+              </Stack>
+            )
+          ) : (
+            ""
+          )}
+        </Box>
+      </Box>
+    </Container>
+  );
+}
