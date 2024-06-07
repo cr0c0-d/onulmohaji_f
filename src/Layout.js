93f7dd60fdbe2034a5bf507f7932fe85654226f6
cr0c0 <hyde69ciel@gmail.com>
2024-06-07 21:52:20 +0900

    #240607 플로팅 버튼 최상단에 위치하도록 수정

--- src/Layout.js
+++ src/Layout.js
@@ -8,33 +8,33 @@ import EventNoteIcon from "@mui/icons-material/EventNote";
 import { useRoute } from "./RouteContext";
 
 function Layout() {
-  const { settingDone } = useUser();
   const { openDrawer, setOpenDrawer } = useRoute();
   return (
     <>
-      {settingDone ? (
-        <div>
-          <Navbar />
+      <div>
+        <Navbar />
+        <br />
+        <Container maxWidth="xl">
+          <Toolbar />
+          <Fab
+            variant="extended"
+            color="primary"
+            sx={{
+              position: "fixed",
+              bottom: 16,
+              right: 16,
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+            }}
+            onClick={() => setOpenDrawer(!openDrawer)}
+          >
+            <EventNoteIcon sx={{ mr: 1 }} />
+            일정 토글
+          </Fab>
+          <Outlet />
           <br />
-          <Container maxWidth="xl">
-            <Toolbar />
-            <Fab
-              variant="extended"
-              color="primary"
-              sx={{ position: "fixed", bottom: 16, right: 16 }}
-              onClick={() => setOpenDrawer(!openDrawer)}
-            >
-              <EventNoteIcon sx={{ mr: 1 }} />
-              일정 토글
-            </Fab>
-            <Outlet />
-            <br />
-          </Container>
-          <Footer />
-        </div>
-      ) : (
-        <div>불러오는중...</div>
-      )}
+        </Container>
+        <Footer />
+      </div>
     </>
   );
 }
