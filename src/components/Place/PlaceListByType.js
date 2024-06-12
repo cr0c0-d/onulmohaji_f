071187ba58d27ac64ce581ac6052a58944b1b27c
cr0c0 <hyde69ciel@gmail.com>
2024-06-12 21:54:04 +0900

    #240612 장소유형별 페이지에 검색창 추가

--- src/components/Place/PlaceListByType.js
+++ src/components/Place/PlaceListByType.js
@@ -1,35 +1,46 @@
-import { Button, Container } from "@mui/material";
+import { Button, Container, Stack } from "@mui/material";
 import PlaceList from "./PlaceList";
 import { useSearchContext } from "../../SearchContext";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { useNavigate } from "react-router-dom";
+import PlaceSearchInfo from "./PlaceSearchInfo";
 
 export default function PlaceListByType({ type }) {
-  const { festival, exhibition, popupstore, facility } = useSearchContext();
+  const { searchInfo, festival, exhibition, popupstore, facility } =
+    useSearchContext();
   const history = useNavigate();
   return (
     <Container>
-      <Button
-        startIcon={<KeyboardArrowLeftIcon />}
-        onClick={() => {
-          history("/");
-        }}
-      >
-        전체 카테고리 보기
-      </Button>
-      <PlaceList
-        placeList={
-          type === "festival"
-            ? festival
-            : type === "exhibition"
-            ? exhibition
-            : type === "popup"
-            ? popupstore
-            : facility
-        }
-        type={type}
-        limit={999}
-      />
+      {searchInfo !== undefined ? (
+        <Stack spacing={5}>
+          <PlaceSearchInfo />
+          <div style={{ display: "flex", justifyContent: "flex-start" }}>
+            <Button
+              startIcon={<KeyboardArrowLeftIcon />}
+              onClick={() => {
+                history("/");
+              }}
+            >
+              전체 카테고리 보기
+            </Button>
+          </div>
+          <PlaceList
+            placeList={
+              type === "festival"
+                ? festival
+                : type === "exhibition"
+                ? exhibition
+                : type === "popup"
+                ? popupstore
+                : facility
+            }
+            type={type}
+            limit={999}
+          />
+        </Stack>
+      ) : (
+        ""
+      )}
     </Container>
   );
 }
