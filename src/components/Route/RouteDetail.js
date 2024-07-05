18af1c4e9f005b22c643510c73fe7911a9906932
cr0c0 <hyde69ciel@gmail.com>
2024-07-05 20:43:41 +0900

    #240705 일정 상세정보 페이지

--- src/components/Route/RouteDetail.js
+++ src/components/Route/RouteDetail.js
@@ -1,12 +1,26 @@
-import { TextField, Box, Chip } from "@mui/material";
+import { TextField, Box, Chip, Typography } from "@mui/material";
+import PlaceInfoSmall from "../Place/PlaceInfoSmall";
 
 export default function RouteDetail({ route }) {
-  <Box>
-    <TextField defaultValue={route.title} label="일정 제목" />
-    {route.memberList && route.memberList.length > 0
-      ? route.memberList.map((memberName, index) => (
-          <Chip label={memberName} variant="outlined" />
-        ))
-      : ""}
-  </Box>;
+  return (
+    <Box>
+      <TextField defaultValue={route.title} label="일정 제목" />
+      {route.memberList && route.memberList.length > 0 ? (
+        <Typography>
+          멤버 목록 :
+          {route.memberList.map((member) => (
+            <Chip label={member.nickname} variant="outlined" key={member.id} />
+          ))}
+        </Typography>
+      ) : (
+        ""
+      )}
+
+      {route !== null
+        ? route.routeDetailList.map((routeDetail) => (
+            <PlaceInfoSmall placeDetail={routeDetail} />
+          ))
+        : ""}
+    </Box>
+  );
 }
