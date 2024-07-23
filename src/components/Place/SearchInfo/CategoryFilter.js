3bd4bed0cc50fbeb84950f390da2c39e5e8d1d59
cr0c0 <hyde69ciel@gmail.com>
2024-07-23 21:49:13 +0900

    #240723 검색조건 관련 컴포넌트 별도 폴더 분리

---
+++ src/components/Place/SearchInfo/CategoryFilter.js
@@ -0,0 +1,41 @@
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Typography,
+} from "@mui/material";
+import { useSearchContext } from "../../../SearchContext";
+import { useEffect, useState } from "react";
+
+export default function CategoryFilter({ categoryFilter, setCategoryFilter }) {
+  return (
+    <div>
+      {categoryFilter ? (
+        <Box>
+          <Typography variant="h6">카테고리 필터</Typography>
+          <FormGroup>
+            {categoryFilter.map((category, index) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={category.visible}
+                    onChange={(e) => {
+                      const updatedArray = [...categoryFilter];
+                      updatedArray[index].visible = e.target.checked;
+                      setCategoryFilter(updatedArray);
+                    }}
+                    name={category.id}
+                  />
+                }
+                label={category.name}
+              />
+            ))}
+          </FormGroup>
+        </Box>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
